744b3d1bba85e7c248eb7c9a32ce1eec6a0f1b4f
ben-yeung <benjaminyeung@berkeley.edu>
2022-10-31 03:35:54 -0700

    Removed extraneous import

--- src/command_template.js
+++ src/command_template.js
@@ -1,17 +1,14 @@
-const Discord = require("discord.js")
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const guildSchema = require('../schemas/guild-schema');
-const botconfig = require('../botconfig.json');
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const botconfig = require("../botconfig.json");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('')
-        .setDescription('')
-        .addStringOption(option => option.setName('').setDescription('').setRequired(true)),
-    options: '',
-    async execute(interaction, args, client) {
-
-        return interaction.reply(':)');
-
-    },
-}
+  data: new SlashCommandBuilder()
+    .setName("")
+    .setDescription("")
+    .addStringOption((option) => option.setName("").setDescription("").setRequired(true)),
+  options: "",
+  async execute(interaction, args, client) {
+    return interaction.reply(":)");
+  },
+};
