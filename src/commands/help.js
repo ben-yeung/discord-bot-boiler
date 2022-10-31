b4339b1eee68079150a71aec7c5ac8483e69d6e3
ben-yeung <benjaminyeung@berkeley.edu>
2022-10-31 03:35:34 -0700

    Removed extraneous import

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,30 +1,25 @@
-const Discord = require("discord.js")
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const guildSchema = require('../schemas/guild-schema');
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Get the list of current commands.'),
-    options: '',
-    async execute(interaction, args, client) {
-        let helpDesc = `View the source code here on [Github](https://github.com/ben-yeung/OS-floor-bot)`;
+  data: new SlashCommandBuilder().setName("help").setDescription("Get the list of current commands."),
+  options: "",
+  async execute(interaction, args, client) {
+    let helpDesc = `These are the current commands available for use!`;
 
-        client.commands.forEach((value, key) => {
-            if (key != 'help')
-                helpDesc += `\n\n **/${key}** ${value.options} \n ${value.data.description}`
-        })
+    client.commands.forEach((value, key) => {
+      if (key != "help") helpDesc += `\n\n **/${key}** ${value.options} \n ${value.data.description}`;
+    });
 
-        let embed = new Discord.MessageEmbed()
-            .setTitle('Help | Active Commands')
-            .setDescription(helpDesc)
-            .setFooter({
-                text: 'Created with ❤️ by ben#0673'
-            })
-            .setColor(44774)
-            .setTimestamp();
+    let embed = new Discord.MessageEmbed()
+      .setTitle("Help | Active Commands")
+      .setDescription(helpDesc)
+      .setFooter({
+        text: "Created with ❤️ by ben#0673",
+      })
+      .setColor(44774)
+      .setTimestamp();
 
-        return interaction.reply({ embeds: [embed], ephemeral: true });
-
-    },
-}
+    return interaction.reply({ embeds: [embed], ephemeral: true });
+  },
+};
